31f6c1dc1bc28648eda40612e689db45fada2bb2
Gabriel <g.izquierdoalcaraz@gmail.com>
2020-04-04 22:12:18 +0200

    :sparkles: Added methods to get 'title' and 'violence'

--- src/scraper.py
+++ src/scraper.py
@@ -27,10 +27,33 @@ class Scraper():
         games.pop(0) # First element contains the table headers
         return games
     
+    def __is_the_game_violent(self, cell, the_game):
+        violence = cell.find('td', 'violence')
+        the_game._violence = bool(violence)
+    
+    def __get_title_from_game(self, cell, the_game):
+        title = cell.find('a').string
+        the_game._title = title
+        
+        self.__is_the_game_violent(cell, the_game)
+        print(the_game)
+    
+    def __get_info_from_game(self, game):
+        the_game = Game()
+        cells = game.findAll('td')
+        
+        # print(cells[1])
+        self.__get_title_from_game(cells[1], the_game)
+        
+    
     def scrape(self):
         print('Web scraping of classic games by "DOSGAMES"...')
         page = self.__download_html(self.url)
         games = self.__get_games_table(page)
+        # for game in games:
+        print(len(games))
+        self.__get_info_from_game(games[0])
+            # break
         
 scraper = Scraper()
 scraper.scrape()
