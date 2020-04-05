5948cc21be9805b04c0b93d8da4823a557bf6221
Gabriel <g.izquierdoalcaraz@gmail.com>
2020-04-05 02:30:04 +0200

    :recycle: Class attributes are set in the init method

--- src/game.py
+++ src/game.py
@@ -1,40 +1,17 @@
 
 class Game(object):
     
-    def __init__(self):
-        self._title = ''
-        self._category = ''
-        self._year = ''
-        self._violence = ''
+    def __init__(self, title, violence, category, year):
+        self._title = title
+        self._violence = violence
+        self._category = category
+        self._year = year
     
     def __str__(self):
-        return 'Game({}, {})'.format(self._title, self._violence)
+        return 'Game({}, {}, {}, {})'.format(self._title, self._violence, self._category, self._year)
     
     def __repr__(self):
         return str(self)
-    
-    @property
-    def title(self):
-        return self._title
-    
-    @title.setter
-    def title(self, title):
-        self._title = title
-          
-    @property
-    def violence(self):
-        return self._violence
-    
-    @violence.setter
-    def violence(self, violence):
-        self._violence = violence
-        
-        
-    def get_category(self):
-        return self.category
-    
-    def set_category(self, value):
-        self.category = value
         
     def get_list(self):
-        return [self._title, self._category]
+        return [self._title, self._category, self._year, self._violence]
